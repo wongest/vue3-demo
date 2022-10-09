6c0b808cc6a75fd5aff267db2003fd138ea1097d
Johnny <345375378@qq.com>
2022-10-09 15:32:43 +0800

    初始化手搭vue3

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="App">
+    num: {{ num }}
+    <div @click="add()">+</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'App',
+  setup() {
+    const num = ref(1);
+    const add = () => {
+      num.value ++
+    }
+    return {
+      num,
+      add,
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.App {
+  color: pink;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const DeleteBuildFile = require('./plugins/deleteBuildFile')
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    // 配置入口文件
+    main: ["@babel/polyfill", path.resolve(__dirname, './src/main.js')],
+  },
+  output: {
+    // 配置打包输出目录
+    path: path.resolve(__dirname, './dist'),
+    // 生成的js 文件名称
+    filename: 'js/[name].[hash:8].js',
+    // 生成的chunk 文件名称
+    chunkFilename: 'js/[name].[hash:8].js',
+  },
+  devServer: {
+    static: './dist',
+    port: 8000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+    ]
+  },
+  plugins: [
+    // new DeleteBuildFile(),
+    new CleanWebpackPlugin(), // 每次打包的时候，都会把 dist 目录清空，和自定义的DeleteBuildFile作用差不多
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html', // 打包后输出的文件名
+      title: '手搭 Vue 开发环境'  // index.html 模板内，通过 <%= htmlWebpackPlugin.options.title %> 拿到的变量
+    }),
+    new VueLoaderPlugin(),
+  ]
+}
